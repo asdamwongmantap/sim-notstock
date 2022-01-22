77610d384d5ec7cb8a4a6b56ad40568ed81cdcee
asdamwongmantap <asdamgrimson@gmail.com>
2022-01-22 10:26:17 +0700

    modify data type int

--- resources/views/stocks/editstock.blade.php
+++ resources/views/stocks/editstock.blade.php
@@ -27,7 +27,7 @@
 					<div class="form-group">
                     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="qty">Qty </label>
                     <div class="col-md-6 col-sm-6 col-xs-12">
-                    	<input type="text" class="form-control col-md-7 col-xs-12" id="qty" name="qty" aria-describedby="qty" value="{{$data_stock->qty}}">
+                    	<input type="number" class="form-control col-md-7 col-xs-12" id="qty" name="qty" aria-describedby="qty" value="{{$data_stock->qty}}">
                     </div>
                   </div>
 				  <div class="form-group">
